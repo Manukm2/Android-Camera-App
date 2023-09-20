0cf4719f566eb680219e4d1acf5d4a6cefd839b8
Manu <manu.km@sirmaindia.com>
2023-09-20 16:45:55 +0530

    First Commit

---
+++ CameraApp/app/src/main/java/com/example/cameraapp/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.cameraapp
+
+import android.Manifest
+
+
+object Constants {
+
+    const val TAG = "cameraX"
+    const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
+    const val REQUEST_CODE_PERMISSION = 123
+    val REQUEST_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+
+}
